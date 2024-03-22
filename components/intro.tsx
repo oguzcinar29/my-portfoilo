5f131177493c49621c60f1ffcc9fd45f4acd2623
Oguz Cinar <marazoguz_29@hotmail.com>
2024-03-22 20:40:27 +0300

    intro text changed

--- components/intro.tsx
+++ components/intro.tsx
@@ -45,12 +45,12 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4  text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Oguz.</span> I'm a{" "}
-        <span className="font-bold">Full-Stack developer </span> I enjoy
+        <span className="font-bold">Full-Stack Web Developer </span> I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
