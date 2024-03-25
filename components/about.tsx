9e3c9f4584dc09aeb5e3acba7623e6473c6f5b5c
Oguz Cinar <marazoguz_29@hotmail.com>
2024-03-25 13:30:28 +0300

    new resume

--- components/about.tsx
+++ components/about.tsx
@@ -19,23 +19,14 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        I'm Oğuz, a former student of electrical engineering who discovered a
-        passion for programming while studying. My journey began with learning C
-        and C++ during my time in university, where I found immense joy in
-        software development. Subsequently, I made the bold decision to
-        transition into the web development sphere, which led me to depart from
-        my university education. In pursuit of becoming a proficient web
-        developer, I devoted myself to self-directed learning and skill
-        enhancement. Through various Udemy courses and acquiring certifications,
-        I deepened my understanding of web technologies. I supplemented
-        theoretical knowledge with practical application, constructing a diverse
-        portfolio of projects that showcased my capabilities. By combining
-        theoretical insights with hands-on experience, I solidified my expertise
-        in web development. I am now adept at creating dynamic and visually
-        appealing websites, driven by a blend of creativity and technical
-        proficiency. I am eager to leverage my skills and passion for web
-        development to contribute effectively to your team and contribute to
-        meaningful projects.
+        I'm Oğuz, an electrical engineering student turned passionate
+        programmer. Starting with C and C++, I found my calling in web
+        development during university. I left formal education to focus on web
+        development, diving into self-learning through Udemy courses and
+        certifications. Building a robust portfolio, I honed my skills in
+        creating dynamic and visually appealing websites. Now, I'm eager to
+        bring my blend of creativity and technical proficiency to your team and
+        contribute to impactful projects.
       </p>
     </motion.section>
   );
